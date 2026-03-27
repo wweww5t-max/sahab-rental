2955ee6226da2f34f20c86ac875a9b4185ddf984
PC <pc@MacBook-Air-RE-ALANAZI.local>
2026-03-27 15:50:06 +0300

    fix seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,13 +10,14 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-   protected $fillable = [
-    'name',
-    'email',
-    'password',
-];
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
-protected $hidden = [
-    'password',
-    'remember_token',
-];
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
