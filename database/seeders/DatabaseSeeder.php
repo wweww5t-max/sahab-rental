c876bc5fc9c383bdcf49d87e54fc9602cc0d1680
PC <pc@MacBook-Air-RE-ALANAZI.local>
2026-03-27 15:56:43 +0300

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,17 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@test.com',
-            'password' => Hash::make('12345678'),
-        ]);
+        //
     }
 }
